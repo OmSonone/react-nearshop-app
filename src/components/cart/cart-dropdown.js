f527bdf97ac531561fb59d4d289816f53c83adc1
bharath sharma <sharmabharath1998@gmail.com>
2020-05-02 01:24:45 +0530

    deploy nai ho rhabut things are working good

--- src/components/cart/cart-dropdown.js
+++ src/components/cart/cart-dropdown.js
@@ -27,7 +27,7 @@ const CartDropdown = ({ cartItems, history, dispatch }) => (
         dispatch(toggleCartHidden());
       }}
     >
-      GO TO CHECKOUT
+      NOTIFY ME!!!
     </CustomButton>
   </div>
 );
