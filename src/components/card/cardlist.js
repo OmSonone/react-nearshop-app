4b5d70affff519965bf3e4422457044b6b184874
Om Sonone <omvsonone2012@gmail.com>
2020-05-01 21:07:59 +0530

    prettier ki koshish

--- src/components/card/cardlist.js
+++ src/components/card/cardlist.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import Card from'./card'
+import React from "react";
+import Card from "./card";
 
-
-const CardList=({robots}) =>{
-	const cardcomponent =robots.map((user,i)=>{
-		return<Card key={i} id ={robots[i].id}  name={robots[i].name} email={robots[i].email}/>
-	})
-	return(<div>
-  	{cardcomponent}
-     </div>
+const CardList = ({ robots }) => {
+	const cardcomponent = robots.map((user, i) => {
+		return (
+			<Card
+				key={i}
+				id={robots[i].id}
+				name={robots[i].name}
+				email={robots[i].email}
+			/>
 		);
-}
+	});
+	return <div>{cardcomponent}</div>;
+};
 export default CardList;
--- src/components/shop/shopview.js
+++ src/components/shop/shopview.js
@@ -1,67 +1,55 @@
-import React,{Component} from 'react';
-import Search from '../search/search.js'
-import Itemlist from './itemlist'
+import React, { Component } from "react";
+import Search from "../search/search.js";
+import Itemlist from "./itemlist";
 
 // import { connect } from 'react-redux';
- 
 
-class Shopview extends Component{
-
-  constructor()
-  {
-      super();            //fr accesing parents and other class memmbers
-      this.state=
-       {
-        robots:[],        //state is used to store all modifying members(dynamic props)
-            searchfield:''
-       }
-
-   }
-      componentDidMount(){
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response=> response.json())
-        .then(users=>this.setState({robots:users}));
-      }
-
-
-      onsearchchange=(event)=>              //fun to to change input value
-      { this.setState({searchfield:event.target.value})
-      }
-
-      
-      render(){
-      const frobots= this.state.robots.filter(robots=>     //take the changed value and change in states robots name
-      {
-              return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-      });
-
-      if(frobots.length===0)
-      {
-        return (<div className='tc'>
-          <Search searchchange={this.onsearchchange}/>
-          <h1 className='tc'>nt found</h1>
-
-          </div>);
-      }
-      else{
-          return(
-        <div className='tc'>
-        
-
-         
-        <Search searchchange={this.onsearchchange}/>
-        <Itemlist robots={frobots}/>
-
-         
-        
-
-        </div>);
-        }
-        
-        
-
+class Shopview extends Component {
+  constructor() {
+    super(); //fr accesing parents and other class memmbers
+    this.state = {
+      robots: [], //state is used to store all modifying members(dynamic props)
+      searchfield: "",
+    };
   }
-  
-      
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
+  }
+
+  onsearchchange = (
+    event //fun to to change input value
+  ) => {
+    this.setState({ searchfield: event.target.value });
+  };
+
+  render() {
+    const frobots = this.state.robots.filter(
+      (
+        robots //take the changed value and change in states robots name
+      ) => {
+        return robots.name
+          .toLowerCase()
+          .includes(this.state.searchfield.toLowerCase());
+      }
+    );
+
+    if (frobots.length === 0) {
+      return (
+        <div className="tc">
+          <Search searchchange={this.onsearchchange} />
+          <h1 className="tc">nt found</h1>
+        </div>
+      );
+    } else {
+      return (
+        <div className="tc">
+          <Search searchchange={this.onsearchchange} />
+          <Itemlist robots={frobots} />
+        </div>
+      );
+    }
+  }
 }
 export default Shopview;
