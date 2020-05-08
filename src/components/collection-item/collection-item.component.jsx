fb228138828b98294922d227ad152601b4866c59
bharath sharma <sharmabharath1998@gmail.com>
2020-05-08 11:12:24 +0530

    refined shop page

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,30 +1,37 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
-import CustomButton from "../form/custom-button.component";
+//import CustomButton from "../form/custom-button.component";
 import { addItem } from "../../redux/cart.actions";
 
-import "./collection-item.styles.scss";
+//import "./collection-item.styles.scss";
 
 const CollectionItem = ({ item, addItem }) => {
   const { name, price, imageUrl } = item;
 
   return (
-    <div className="collection-item  grow ">
-      <div
-        className="image "
-        style={{
-          backgroundImage: `url(${imageUrl})`,
-        }}
+      <div className="  dib br2   pa3 mh3  bw2 shadow-5 tc card">
+      <img
+        className="br2 shadow-5 grow dib br3  "
+        alt="items"
+        src={`${imageUrl} `}
       />
-      <div className="collection-footer">
-        <span className="name"> {name}</span>
-        <span className="price">${price}</span>
+      <div>
+        <h2>{name}</h2>
+        <h3>${price}</h3>
+        <button
+          to=""
+          onClick={() => addItem(item)}
+          className="f6 grow no-underline br-pill  ba  ph3  pv2 ph5 mb1 dib near-black hover-bg-black hover-white shadow-5"
+        >
+         Add to cart
+        </button>
       </div>
-      <CustomButton onClick={() => addItem(item)} inverted>
-        Add to cart
-      </CustomButton>
     </div>
+
+
+    
   );
 };
 
